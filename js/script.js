78e1790138519f2e4e2f6b2e05ceac7837a893c5
Shohrat-Code <shohrat@code.edu.az>
2021-10-19 23:16:15 +0400

    Codes

---
+++ js/script.js
@@ -0,0 +1,138 @@
+// var Name = "Rasim";
+// console.log(Name);
+
+// let Surname = "Abbasov";
+// console.log(Surname);
+
+
+// if (true) {
+//     var a = 15;
+//     let b = 20;
+// }
+
+// console.log(a);
+// console.log(b);
+
+// function test() {
+//     if (true) {
+//         var age = 52;
+//     }
+// }
+
+// test();
+// console.log(age);
+
+
+// if (a > 5) {
+
+// } else if (a == 5) {
+
+// } else {
+
+// }
+
+
+// if (a > 5) {
+
+// }
+// if (a == 5) {
+
+// }
+// if (a < 5) {
+
+// }
+
+// 'use strict'
+// let a;
+
+// console.log(a != null ? a : "No data");
+// console.log(a ?? "No data");
+
+// let count = +prompt("Enter the count:");
+// if (confirm("Are you sure to colsole the numbers?")) {
+//     for (let i = 0; i < count; i++) {
+//         console.log(i)
+//     }
+// }
+
+
+// for (let o = 0; o < 10;) {
+//     // if (o == 4) {
+//     //     break;
+//     // }
+
+//     console.log(o);
+// }
+
+// let i = 0;
+// while (i < 10) {
+//     i++;
+//     console.log(i);
+// }
+
+// do {
+//     console.log(i);
+//     i++;
+// } while (i < 10);
+
+
+// console.log(ShowMessage("sdfkj"));
+
+
+
+// function ShowMessage(message, m2 = "Default") {
+//     return message + " " + m2;
+//     // console.log(message + " " + m2);
+// }
+
+// let result = ShowMessage("Salam Rasim!", "Necesen?");
+// console.log(result);
+
+// function MultipleParam(...params) {
+//     // console.log(params)
+
+//     for (let i = 0; i < params.length; i++) {
+//         console.log(params[i]);
+//     }
+//     // console.log(params[0]);
+//     // console.log(params[1]);
+//     // console.log(params[2]);
+// }
+
+// MultipleParam("Rasim", 25, 35, "Salam", true, null, 654);
+// let ShowMessage;
+
+// 'use strict'
+
+// if (true) {
+//     // ShowMessage = function (message) {
+//     //     console.log(message);
+//     // };
+
+//     function ShowMessage(message) {
+//         console.log(message);
+//     }
+// }
+
+// let ShowMessage = (message, m2) => message + " " + m2;
+
+// console.log(ShowMessage("Salam", "Rasim"));
+
+// let a = Number(prompt("Enter a:"));
+// let b = Number(prompt("Enter b:"));
+
+// if (a == 50 || b == 50 || (a + b) == 50) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// let sum = 0;
+
+// for (let i = 15; i <= 1000; i++) {
+//     if (i % 15 == 0) {
+//         sum += i;
+//     }
+// }
+
+// console.log(sum);
